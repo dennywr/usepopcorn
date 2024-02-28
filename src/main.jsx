a51e4c5ec7467e0acb1f87803ffd3e62d8a86481
Denny Wahyudi Ramadhon <dennywahyudiramadhon@gmail.com>
2024-02-28 11:00:04 +0700

    create a StarRating component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import StarRating from './components/StarRating';
+import { useState } from 'react';
+// import App from './App.jsx'
+// import './index.css'
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating maxRating={5} size={50} onSetRating={setMovieRating} />
+      <span>The rating was {movieRating}</span>
+    </div>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <StarRating maxRating={5} size={30} message={['terrible', 'bad', 'not bad', 'good', 'amazing']} />
+    <StarRating maxRating={5} color="red" size={50} />
+    <Test />
+  </React.StrictMode>
+);
